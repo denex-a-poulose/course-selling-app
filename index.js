b439a08be5742048cc9056df6a04b5e327738497
Denex A Poulose <denexpoulose@gmail.com>
2024-12-11 11:04:05 +0530

    added the schema

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+console.log(process.env.MONGO_USER_PASSWORD);
+
 const userRouter = require("./routes/user");
 const courseRouter = require("./routes/course");
 const adminRouter = require("./routes/admin");
@@ -8,6 +12,16 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
-app.listen(3006, () => {
-  console.log("Listening on port 3006");
-});
+const main = async () => {
+  mongo_user_password = process.env.MONGO_USER_PASSWORD;
+
+  await mongoose.connect(
+    `mongodb+srv://denex-mongo:${mongo_user_password}@cluster0.abc6y.mongodb.net/course-app`
+  );
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+};
+
+main();
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Router = express.Router;
-
+const { adminModel } = require("../db");
 const adminRouter = Router();
 
 // adminRouter.use(adminMiddleware);
